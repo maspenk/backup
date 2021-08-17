6d131533a2a156830ae169047ceab64783ca6606
maspenk <lutfideveloper@gmail.com>
2021-08-17 09:33:17 +0700

    Delete wlan.sh

--- dwm-status/wlan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-wlan=$(iwctl station wlan0 show | grep 'Connected network' | awk '{$1=$2=""; print substr($0,3)}')
-
-if [ $wlan != "" ]; then
-    echo " NET:$wlan"
-else
-    echo " NET:-"
-fi    
