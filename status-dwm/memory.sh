e133b784cba926fa806bfbe9d69cfdf2f0e04079
maspenk <lutfideveloper@gmail.com>
2022-01-11 11:12:12 +0700

    Update memory.sh

--- status-dwm/memory.sh
+++ status-dwm/memory.sh
@@ -6,7 +6,7 @@ mem_avail=$(free -m | awk 'NR%2==0 {print $7}')
 mem_used=$(( $mem_total - $mem_avail ))
 mem_usage=$(( $mem_used * 100 / $mem_total ))
 if [ $mem_usage -ge 80 ]; then
-    printf "$icon${mem_usage//%}!"
+    printf "$icon${mem_usage}!"
 else
-    printf "$icon${mem_usage//%}"
+    printf "$icon${mem_usage}"
 fi
